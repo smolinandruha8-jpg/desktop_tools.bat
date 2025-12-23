107a2074aa64b7dfd17b645a4bf596ce07a5d45d
smolinandruha8-jpg <smolinandruha8@gmail.com>
2025-12-23 22:37:13 +0500

    V2025.6 BIG UPDATE
    NEW KEY CODE "TIC-TAC-TOE" & MORE

--- desktop_tools.bat
+++ desktop_tools.bat
@@ -11,7 +11,7 @@ cls
 :bdb
 cls
 @echo off
-echo /=================v2025.4===========(Default)==========================/
+echo /=================v2025.6===========(Default)==========================/
 echo Welcome to desktop tools
 echo Key codes:
 echo =======================================================================
@@ -23,8 +23,7 @@ echo 5. run ALL files (be careful!)
 echo 6. kalendar
 echo 7. run all files (with cooldown)
 echo 8. make file as shell:startup
-echo 9. check the .bat file for suspicious manipulations
-echo 10.
+echo 10. tic-tac-toe
 echo /=======================================================================/
 echo vx - credits
 
@@ -78,26 +77,57 @@ if %keycode% equ 8 (
 set /p pt=Enter path to your file
 move %pt% %USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 )
-if %keycode% equ 9 (
-  cls
-  :: var
-  set /p pth=Enter path to your .bat file
-  :: eth
-  findstr /C:"shutdown -s -t N" pth
-  findstr /C:"dir/s" pth
-  findstr /C:"color a" pth
-  findstr /C:"color 2" pth
-  :: main
-  echo if you see text like "dir/s" or "shutdown" this file is suspicious
-  pause
+if %keycode% equ 10 (
+    goto :start_game
+) else (
+    goto :bdb
 )
 
-if %keycode% equ "vx" (
-  cls
-  echo ==============================
-  echo Script editor - DevTO
-  echo ==============================
-  echo
-  pause
-)
-goto bdb
+:start_game
+cls
+set /p "user1=[PLAYER 1] Username: "
+set /p "user2=[PLAYER 2] Username: "
+
+set "a1=n" & set "a2=n" & set "a3=n"
+set "b1=n" & set "b2=n" & set "b3=n"
+set "c1=n" & set "c2=n" & set "c3=n"
+
+:ttt
+cls
+echo   a  b  c
+echo 1[%a1%][%b1%][%c1%]
+echo 2[%a2%][%b2%][%c2%]
+echo 3[%a3%][%b3%][%c3%]
+echo.
+
+set /p "usp1=%user1%, enter cell (like a1 or b2): "
+if /i "%usp1%"=="a1" if "%a1%"=="n" set "a1=X"
+if /i "%usp1%"=="a2" if "%a2%"=="n" set "a2=X"
+if /i "%usp1%"=="a3" if "%a3%"=="n" set "a3=X"
+if /i "%usp1%"=="b1" if "%b1%"=="n" set "b1=X"
+if /i "%usp1%"=="b2" if "%b2%"=="n" set "b2=X"
+if /i "%usp1%"=="b3" if "%b3%"=="n" set "b3=X"
+if /i "%usp1%"=="c1" if "%c1%"=="n" set "c1=X"
+if /i "%usp1%"=="c2" if "%c2%"=="n" set "c2=X"
+if /i "%usp1%"=="c3" if "%c3%"=="n" set "c3=X"
+
+cls
+echo   a  b  c
+echo 1[%a1%][%b1%][%c1%]
+echo 2[%a2%][%b2%][%c2%]
+echo 3[%a3%][%b3%][%c3%]
+echo.
+echo %user1% make his move
+
+set /p "usp2=%user2%, enter cell (like a1 or b2): "
+if /i "%usp2%"=="a1" if "%a1%"=="n" set "a1=0"
+if /i "%usp2%"=="a2" if "%a2%"=="n" set "a2=0"
+if /i "%usp2%"=="a3" if "%a3%"=="n" set "a3=0"
+if /i "%usp2%"=="b1" if "%b1%"=="n" set "b1=0"
+if /i "%usp2%"=="b2" if "%b2%"=="n" set "b2=0"
+if /i "%usp2%"=="b3" if "%b3%"=="n" set "b3=0"
+if /i "%usp2%"=="c1" if "%c1%"=="n" set "c1=0"
+if /i "%usp2%"=="c2" if "%c2%"=="n" set "c2=0"
+if /i "%usp2%"=="c3" if "%c3%"=="n" set "c3=0"
+
+goto :ttt
